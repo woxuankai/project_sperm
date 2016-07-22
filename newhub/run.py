efe693f5f714a5d08f7b1c214bffa26d1b925c8a
Kyle Xuan <woxuankai@gmail.com>
2016-07-22 22:31:17 +0800

    continue to work if failed to upload

--- newhub/run.py
+++ newhub/run.py
@@ -5,43 +5,44 @@ configfilepath = './config.json'
 
 import os, time
 from getnodes import getnodes
-from startnode import startnode
+from startnode import startnode, startnodetest
 if __name__ == '__main__':
-    print('program start!')
-    #get nodes info
-    try:
-        nodes=getnodes(configfilepath);
-    except Exception as e:
-        print('fail to  parse file: ',configfilepath)
-        print('details: ',e)
-    #one node, one process
-    childpids = {};
-    while len(nodes) >= 1:
-        nodename,nodeinfo = nodes.popitem()
-        pid = os.fork()
-        if pid == 0:
-            #child process
-            try:
-                nodeinfo['nodename'] = nodename
-                startnode(nodeinfo)
-            except Exception as e:
-                print('failed to start node: {}'.format(nodename))
-                print('details: ',e)
-                exit(-1)
-            #shouldn't reach here
-            exit(0)
-        else:
-            #parent process
-            childpids[pid]=nodename
-            time.sleep(0.5)
-            #continue to fork
-    #forked one process per node
-    #wait for every process to finish
-    while len(childpids) >= 1:
-        pid,status = os.wait()
-        exitchildname = childpids.pop(pid);
-        print('process for node <{}> exited'.format(exitchildname))
-        print('exit status indication: ',status)
-    #child processes should never return
-    #thus program shouldn't come here
-    exit(0)
+	print('program start!')
+	#get nodes info
+	try:
+		nodes=getnodes(configfilepath);
+	except Exception as e:
+		print('fail to  parse file: ',configfilepath)
+		print('details: ',e)
+	#one node, one process
+	childpids = {};
+	while len(nodes) >= 1:
+		nodename,nodeinfo = nodes.popitem()
+		pid = os.fork()
+		if pid == 0:
+			#child process
+			try:
+				nodeinfo['nodename'] = nodename
+				startnode(nodeinfo)
+				#startnodetest(nodeinfo)
+			except Exception as e:
+				print('failed to start node: {}'.format(nodename))
+				print('details: ',e)
+				exit(-1)
+			#shouldn't reach here
+			exit(0)
+		else:
+			#parent process
+			childpids[pid]=nodename
+			time.sleep(0.5)
+			#continue to fork
+	#forked one process per node
+	#wait for every process to finish
+	while len(childpids) >= 1:
+		pid,status = os.wait()
+		exitchildname = childpids.pop(pid);
+		print('process for node <{}> exited'.format(exitchildname))
+		print('exit status indication: ',status)
+	#child processes should never return
+	#thus program shouldn't come here
+	exit(0)
--- newhub/startnode.py
+++ newhub/startnode.py
@@ -4,29 +4,41 @@
 from thread_uploaddata import uploaddata
 import serial, threading
 def startnode(nodeinfo):
-    #try:
-    #    ser = serial.Serial(nodeinfo['devname'],nodeinfo['baudrate'])
-    #except Exception as e:
-    #    print('failed to open serial port!')
-    #    raise e
-    ser = serial.Serial(nodeinfo['devname'],nodeinfo['baudrate'])
-    #successfully opened serial port, start to listen and report
-    #flush input
-    ser.flushInput()
-    #ingore bad line
-    data = ser.readline()
-    while True:
-        #now listen
-        try:
-            data = ser.readline()
-            data = data.decode('utf-8')
-        except Exception as e:
-            print('error occured while reading data from serial port')
-            print('node: {}'.format(nodeinfo['nodename']))
-            print('details:\n',e)
-            continue
-        #start a new thread to upload data
-        t = threading.Thread(target=uploaddata,args=(data,nodeinfo))
-        t.start()
-    #shouldn't come here if everything is ok
-    #return 0
+	#try:
+	#	ser = serial.Serial(nodeinfo['devname'],nodeinfo['baudrate'])
+	#except Exception as e:
+	#	print('failed to open serial port!')
+	#	raise e
+	ser = serial.Serial(nodeinfo['devname'],nodeinfo['baudrate'])
+	#successfully opened serial port, start to listen and report
+	#flush input
+	ser.flushInput()
+	#ingore bad line
+	data = ser.readline()
+	while True:
+		#now listen
+		try:
+			data = ser.readline()
+			data = data.decode('utf-8')
+		except Exception as e:
+			print('error occured while reading data from serial port')
+			print('node: {}'.format(nodeinfo['nodename']))
+			print('details:\n',e)
+			continue
+		#start a new thread to upload data
+		t = threading.Thread(target=uploaddata,args=(data,nodeinfo))
+		t.start()
+	#shouldn't come here if everything is ok
+	#return 0
+
+
+
+import time
+def startnodetest(nodeinfo):
+	data='	  5.6234				 '
+	#start a new thread to upload data
+	while True:
+		t = threading.Thread(target=uploaddata,args=(data,nodeinfo))
+		t.start()
+		print("pretend received data")
+		time.sleep(1)
