8f9da4de03b3deb1aaf15cbc73a6c3c1506c5715
Kyle Xuan <woxuankai@gmail.com>
2016-07-28 23:30:14 +0800

    finished module cam

--- newhub/mods/mod_cam.py
+++ newhub/mods/mod_cam.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-configfilepath = './config_img.json'
 
 import pygame, pygame.camera
 from urllib import request, parse
 import json
 
 def caponeframe(imgpath, dev, resolution):
-	try:	
-		pygame.camera.init()
-		cam = pygame.camera.Camera(dev,resolution)
-		cam.start()
-		img = cam.get_image()
-		cam.stop()
-	except Exception as e:
-		print('capture failed')
-		print('details: ',e)
-		raise
-	try:
-		pygame.image.save(img,imgpath)
-	except Exception as e:
-		print('save image failed')
-		print('details: ',e)
-		raise
+	pygame.camera.init()
+	cam = pygame.camera.Camera(dev,resolution)
+	cam.start()
+	img = cam.get_image()
+	cam.stop()
+	pygame.image.save(img,imgpath)
 	return 0
 
 def uploadoneframe(serveraddr,imgpath):
@@ -39,50 +28,47 @@ def uploadoneframe(serveraddr,imgpath):
 		#resdict = json.loads(res.decode('utf-8'))
 	return res.decode('utf-8')
 
-import datetime
-def gettimestr():
-	return str(datetime.datetime.now())
-
-import json
-def getconfig(filepath):
-    with open(filepath, 'r') as f:
-        s = f.read()
-    config = json.loads(s)
-    return config
 
 import time, os
-if __name__ == '__main__':
-	if configfilepath[0] != '/':
-		BASE_DIR = os.path.dirname(__file__)
-		configfilepath = os.path.join(BASE_DIR, configfilepath)
-	config = getconfig(configfilepath)
-	addr = config['addr']
-	imgfilepath = config['imgfilepath']
-	timeinterval = config['timeinterval']
-	videodevice = config['videodevice']
-	resolution = config['resolution']
-	if imgfilepath[0] != '/':
-		BASE_DIR = os.path.dirname(__file__)
-		imgfilepath = os.path.join(BASE_DIR, imgfilepath)
-	timenext = time.time()
-	while(1):
-		timenext = timenext + timeinterval
-		#capture and save a frame
-		print(gettimestr(),': session start')
-		caponeframe(imgfilepath, videodevice, resolution)
-		print(gettimestr(),': captured')
-		#upload a frame
-		#result = uploadoneframe(addr, "../testimgs/1.jpg")
-		#result = uploadoneframe(addr, "./postdata.py")
-		#print(result)
+def run(config, logger):
+	logger.info('this is func run of mod_cam')
+	try:
+		addr = config['addr']
+		imgfilepath = config['imgfilepath']
+		timeinterval = config['timeinterval']
+		videodevice = config['videodevice']
+		resolution = config['resolution']
+	except:
+		logger.exception('missing config para')
+		exit(1)
+	cnt = 0
+	timenext = time.time()#just put it here to save a try--except
+	while True:
+		cnt = cnt + 1
+		try:
+			sleeptime = timenext - time.time()
+			if sleeptime < 0:
+				sleeptime = 0
+			time.sleep(sleeptime)
+			timenext = timenext + timeinterval
+		except:
+			logger.exception('failed to sleep')
+			exit(1)
+		try:
+			#capture and save a frame
+			caponeframe(imgfilepath, videodevice, resolution)
+		except:
+			logger.exception('#{} failed to capture'.format(cnt))
+			continue
+		logger.info('#{} captured'.format(cnt))
+		#	upload a frame
+		#	no need to fork
 		try:
 			result = uploadoneframe(addr, imgfilepath)
-		except Exception as e:
-			print(gettimestr(),': failed to upload')
-		else:
-			print(gettimestr(),': uploaded')
-			print('result: ',result)
-		sleeptime = timenext-time.time()
-		if sleeptime < 0:
-			sleeptime = 0
-		time.sleep(timenext-time.time())
+		except:
+			logger.exception('#{} failed to upload'.format(cnt))
+			continue
+		logger.info('#{} uploaded, result: {}'.format(cnt, result))
+	#end of loop
+	exit(1)
+	return
