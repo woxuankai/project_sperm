a31929208349596948ed7c1d438c0104d1381f41
Kyle Xuan <woxuankai@gmail.com>
2016-08-08 19:07:18 +0800

    more flexibility in test scales mod

--- newhub/mods/test_mod_scales.py
+++ newhub/mods/test_mod_scales.py
@@ -4,9 +4,10 @@
 
 import time
 import serial
+import sys
 if __name__ == '__main__':	
 	while True:	
-		ser = serial.Serial('/dev/pts/5', 9600)
+		ser = serial.Serial(sys.argv[1], 9600)
 		ser.write('ST,GS,+  4.876kg\r\n'.encode('utf-8'))
 		ser.close()
 		time.sleep(0.5)
